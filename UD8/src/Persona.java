78028ddf5ea34900a93e5feb78de9ea22ad263da
Ainara <ainarabliz@gmail.com>
2023-10-06 16:55:24 +0200

    Primera conexión con el git

---
+++ UD8/src/Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	private String dni;
+	private char sexo = 'H';
+	private int peso;
+	private int altura;	
+
+
+private final char SEXODEFECTO = 'H';
+	
+public void hm() {
+		if (sexo !='H' && sexo != 'M') {
+			this.sexo = SEXODEFECTO;	
+			}
+
+}	
