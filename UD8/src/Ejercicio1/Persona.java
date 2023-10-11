8e5df0a61923fd1a403ef67a9d96beb05c234b13
Ainara <ainarabliz@gmail.com>
2023-10-11 12:45:41 +0200

    Seguir después de comer: arreglado los atributos

--- UD8/src/Ejercicio1/Persona.java
+++ UD8/src/Ejercicio1/Persona.java
@@ -1,19 +1,19 @@
-/*package Ejercicio1;
+package Ejercicio1;
 
 public class Persona {
-	private String nombre;
-	private int edad;
-	private String dni;
-	private char sexo = 'H';
-	private int peso;
-	private int altura;	
+	protected String nombre;
+	protected int edad;
+	protected String dni;
+	protected char sexo = 'H';
+	protected int peso;
+	protected int altura;	
 
 
-private final char SEXODEFECTO = 'H';
+	protected final char SEXODEFECTO = 'H';
 	
-public void hm() {
+/*public void hm() {
 		if (sexo !='H' && sexo != 'M') {
 			this.sexo = SEXODEFECTO;	
-			}
+			}*/
 
-}	*/
+}	
